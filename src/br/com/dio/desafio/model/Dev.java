30a6b9c3b81dbb68097d5ffe7fe644538b776712
brunosas88 <bruno.sas@live.com>
2022-07-31 16:13:26 -0300

    feat: adicionadas classes Bootcamp.java e Dev.java

---
+++ src/br/com/dio/desafio/model/Dev.java
@@ -0,0 +1,36 @@
+package br.com.dio.desafio.model;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+
+    }
+
+    public void progredir() {
+
+    }
+
+    public void calcularTotalXp() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+}
